b6ac06deb582da3b49100a627425e00468e55875
M.Minhaj <minhajmuhammad265@gmail.com>
2025-07-05 15:17:55 +0500

    Add files via upload

---
+++ 03_calculate_average.py
@@ -0,0 +1,11 @@
+def av():
+    x= int(input("Enter your number: "))
+    y= int(input("Enter your number: "))
+    z= int(input("Enter your number: "))
+   
+    average=(x + y + z) / 3
+    print(average)
+ # function call
+av()
+av()
+av()
